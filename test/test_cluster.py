cd8c6768f5330cf26d996beabe6e5f500cb6b00f
Johnny Yu <johnny.yu@ucsf.edu>
2019-02-08 16:42:10 -0800

    3

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -20,23 +20,7 @@ def test_partition_clustering():
     for id in pdb_ids:
         filepath = os.path.join("data", "%i.pdb"%id)
         active_sites.append(io.read_active_site(filepath))
-    ####format answer
-    #clust1 = [276]
-    #clust2 = [4629, 10701]
-    ##
-    #answer = []
-    #tmp1 = []
-    #for id in clust1:
-   #     filepath = os.path.join("data", "%i.pdb"%id)
-   #     tmp1.append(io.read_active_site(filepath))
-   # tmp2 = []
-   # for id in clust2:
-  #      filepath = os.path.join("data", "%i.pdb"%id)
-   #     tmp2.append(io.read_active_site(filepath))
-   # answer.append(tmp1)
-   # answer.append(tmp2)
-    # update this assertion
-    assert test_format(cluster.cluster_by_partitioning(active_sites,2)) == [[276], [4629, 10701]]
+    assert cluster.test_format(cluster.cluster_by_partitioning(active_sites,2)) == [[276], [4629, 10701]]
 
 
 def test_hierarchical_clustering():
@@ -49,5 +33,5 @@ def test_hierarchical_clustering():
         active_sites.append(io.read_active_site(filepath))
 
     # update this assertion
-    assert cluster.cluster_hierarchically(active_sites) == [[[276, 4629], [10701]], [[276, 4629, 10701]]]
+    assert cluster.test_format(cluster.cluster_hierarchically(active_sites)) == [[[276, 4629], [10701]], [[276, 4629, 10701]]]
 
